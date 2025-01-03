f8de8b7ab76cb577d0f887b1fe50576632773983
taktiks2 <deathproof.lee@gmail.com>
2025-01-03 22:49:01 +0900

    単一のsprite追加

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, (setup_camera, setup_sprite))
+        .run();
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn(Camera2d);
+}
+
+fn setup_sprite(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn((
+        Sprite {
+            image: asset_server.load("tak-cat.png"),
+            custom_size: Some(Vec2::new(50., 50.)),
+            ..default()
+        },
+        Transform::from_xyz(10., 0., 0.),
+    ));
 }
